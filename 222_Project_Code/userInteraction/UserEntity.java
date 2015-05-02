c270083c8d22f7aa55b401eca73a54264a6ad482
lijicheng0036 <lijicheng0036@gmail.com>
2015-05-02 15:27:30 +1000

    updates_codes

--- 222_Project_Code/userInteraction/UserEntity.java
+++ 222_Project_Code/userInteraction/UserEntity.java
@@ -1,18 +1,86 @@
 package userInteraction;
 
 import java.util.*;
+import java.io.*;
 
 public class UserEntity{
 	
-	private username;
-	private password;
-	private role;
-
-	public UserEntity(){
-
+	private BufferedReader reader;	
+	private PrintWriter writer;
+	private File file;
+	private String dbfile = "userAccount.csv";
+	private String passwdInDB;
+	private String role;
+	
+	public String login(String username, char[] password){
+		String passwd = new String(password);
+		String oneLine = "";
+		boolean existed = false;
+		
+		try{
+			reader = new BufferedReader(new FileReader(dbfile));
+			while(!existed && ((oneLine = reader.readLine()) != null)){
+			// oneLine represents one line in the DB; Recursively check the DB;	
+                String[] words = oneLine.split(",");
+				
+				if(username.equals(words[0])){
+					existed = true;
+					passwdInDB = words[1];
+					
+					if(passwd.equals(passwdInDB)){
+						role = word[2];	//If username and password are correct,return role;	
+					}else{
+						role = "loginFail";	//If username exists, but password is wrong,login fails;
+					}	
+				}
+				
+            }
+			
+			if(!existed){
+				role = "loginFail";		//If the username doesn't exist, login fails;
+			}
+			
+            reader.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return role;
 	}
 
-	public String login(String username, char[] password){
+	public boolean signUp(String username, char[] password,String role){
+		String passwd = new String(password);
+		String oneLine = "";
+		boolean existed = false;
 		
-	}	
-}
+		//At first, check whether the account exists already in the DB;
+		try{
+			reader = new BufferedReader(new FileReader(dbfile));
+			while(!existed && ((oneLine = reader.readLine()) != null)){
+                String[] words = oneLine.split(",");
+				
+				if(username.equals(words[0])){
+					existed = true;
+					return false;	
+				}
+				
+            }
+			
+            reader.close();		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		//If the account doesn't exist in the DB, then insert the username/password/role into the DB;
+		try{
+			writer = new PrintWriter(file);
+			writer.println(username + "," + passwd + "," + role);
+            writer.close();		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+};
