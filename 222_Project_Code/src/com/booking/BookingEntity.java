0c9b5f37aec4742f7f0d6b9876f75f64bf6d5524
lijicheng0036 <lijicheng0036@gmail.com>
2015-05-23 15:08:40 +1000

    saveBooking
    services_booked.csv
    tickets.csv
    booking.csv

--- 222_Project_Code/src/com/booking/BookingEntity.java
+++ 222_Project_Code/src/com/booking/BookingEntity.java
@@ -23,6 +23,9 @@ public class BookingEntity {
 	
 	private String discountFile = System.getProperty("user.dir") + File.separator + "database" + File.separator + "discount.csv";
 	private String bookingFile = System.getProperty("user.dir") + File.separator + "database" + File.separator + "booking.csv";
+	private String ticketsFile = System.getProperty("user.dir") + File.separator + "database" + File.separator + "tickets.csv";
+	private String serviceFile = System.getProperty("user.dir") + File.separator + "database" + File.separator + "services_booked.csv";
+	
 	private BufferedReader reader;
 	private PrintWriter writer;
 	
@@ -30,8 +33,72 @@ public class BookingEntity {
 		
 	}
 	
-	public void saveBooking(String flight_id, List<Ticket> tickets, List<ServiceBooking> services_booked){
+	public static void saveBooking(String flight_id, List<Ticket> tickets, List<ServiceBooking> services_booked){	
+		//First step, save the booking info into booking.csv.
+		int bk_id = 0;	//If no record in the file, default value is 0; It will be incremented by 1 if a record is inserted;
+		String booking_status = "active";
+		int tnum = tickets.size();			//Count for tickets totally;
+		double total_price = 0.0;			//The total price for all the tickets in the booking;
+		String oneLine = "";				//The temporary string for reading from the database file line by line;
 		
+		for(int p=0;p < tnum;p++){
+			total_price += tickets.get(p).getPrice(); 			
+		}
+		
+		try{
+			reader = new BufferedReader(new FileReader(bookingFile));			
+			while(((oneLine = reader.readLine()) != null)){
+                String[] words = oneLine.split(",");
+				bk_id = Integer.parseInt(words[0]); 				
+            }			
+            reader.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		//bookingID + flightID + Status + Total_Price -- in the booking.csv
+		try{
+			bk_id++;		// Increment the booking id by 1, for the next booking record;
+			writer = new PrintWriter(new FileOutputStream(new File(bookingFile),true));		//To append to the file using "true";
+			writer.print(bk_id + "," + flight_id + "," + total_price + ",");
+			writer.println(booking_status);						   
+			writer.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		//Second step, save the tickets info into tickets.csv, one booking contains many tickets;		
+		for(int i=0;i < tnum;i++){		//tnum is the amount of tickets;
+			int ticket_id = tickets.get(i).getTicketId();
+			String uname = tickets.get(i).getUsername();
+			int person_id = tickets.get(i).getPersonId();
+			String seat_number = tickets.get(i).getSeatNumber();
+			
+			//ticketID + userName + personID + bookingID + seatNumber -- in the tickets.csv
+			try{ 
+				writer = new PrintWriter(new FileOutputStream(new File(ticketsFile),true));		//To append to the file using "true";
+				writer.println(ticket_id + "," + uname + "," + person_id + "," + bk_id + "," + seat_number);					   
+				writer.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		//Third step, save the services info into services_booked.csv, one ticket may contain many services;
+		int snum = services_booked.size();		//Count for services totally;
+		for(int k=0;k < snum;k++){
+			int ticketId = services_booked.get(k).getTicketId();
+			int serviceId = services_booked.get(k).getServiceId();
+			
+			//ticketID + serviceID -- in the services_booked.csv
+			try{ 
+				writer = new PrintWriter(new FileOutputStream(new File(serviceFile),true));		//To append to the file using "true";
+				writer.println(ticketId + "," + serviceId);					   
+				writer.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void setDiscountRatio(double ratio){
