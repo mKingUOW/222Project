21a2afd1f5e6353ecd11c3c708469fe8ce14195f
lijicheng0036 <lijicheng0036@gmail.com>
2015-05-24 11:03:08 +1000

    updateAvailableSeats
    updateAvailableSeats

--- 222_Project_Code/src/com/booking/FlightEntity.java
+++ 222_Project_Code/src/com/booking/FlightEntity.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class FlightEntity{
 	
-	private String filepath = System.getProperty("user.dir") + File.separator + "database" + File.separator + "flight-route-schedule.csv";
+	private String scheduleFile = System.getProperty("user.dir") + File.separator + "database" + File.separator + "flight-route-schedule.csv";
 	private BufferedReader reader;
 	
 	public FlightEntity(){
@@ -19,8 +19,11 @@ public class FlightEntity{
 		String oneLine = "";
 		String abbreviation = "";
 		
+		//Attributes in the flight-route-schedule.csv:
+		//AUTO_INCREMENT_id + flight_id + plane_id + route_number + depart_time + arrive_time
+		// + First_Class_SeatS + Business_Class_Seats + Premium_Economic_Seats + Economic_Class_Seats;
 		try{
-			reader = new BufferedReader(new FileReader(filepath));
+			reader = new BufferedReader(new FileReader(scheduleFile));
 			while((oneLine = reader.readLine()) != null){
 				
 				String [] words = oneLine.split(",");
@@ -48,7 +51,50 @@ public class FlightEntity{
 	}
 	
 	public void updateAvailableSeats(String flight_id, int[] available_seats){
+		int fcSeat = available_seats[0];
+		int bcSeat = available_seats[1];
+		int pecSeat = available_seats[2];
+		int ecSeat = available_seats[3];
 		
+		String oneLine = "";
+		String data = "";
+		String updatedLine = "";
+		
+		try{
+			reader = new BufferedReader(new FileReader(scheduleFile));
+			while((oneLine = reader.readLine()) != null){
+                String[] words = oneLine.split(",");
+				
+				tmp_flight_id = words[1];
+				if(flight_id.compare(tmp_flight_id) == 0){
+					for(int i=0;i < 6;i++){
+						updatedLine += words[i];					// until arrive_time, which is words[5], no need to modify;
+						updatedLine += ",";
+					}
+					updatedLine += Integer.toString(fcSeat);		// Update First_Class_Seats;
+					updatedLine += ",";
+					updatedLine += Integer.toString(bcSeat);		// Update Business_Class_Seats;
+					updatedLine += ",";
+					updatedLine += Integer.toString(pecSeat);		// Update Premium_Economic_Seats;
+					updatedLine += ",";
+					updatedLine += Integer.toString(ecSeat);		// Update Economic_Class_Seats;
+					data += updatedLine + "\n";						// Add the modified record into data;
+				}else{
+					data += oneLine + "\n";							// If the flight_id doesn't match, add the original record in the database into data;
+				}	
+            }
+		     reader.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		try{
+			writer = new PrintWriter(new FileOutputStream(new File(scheduleFile)));	
+			writer.print(data);
+            writer.close();	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	/**
